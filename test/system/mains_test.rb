b8db8b85c3e0c75091afa572f1ec421073663c68
pakornyodkab <yodpakorn2543@gmail.com>
2021-10-31 16:19:23 +0700

    do follow test already

--- test/system/mains_test.rb
+++ test/system/mains_test.rb
@@ -3,6 +3,7 @@ require "application_system_test_case"
 class MainsTest < ApplicationSystemTestCase
   setup do
     @user = users(:u1)
+    @user2 = users(:u2)
   end
   
 
@@ -28,6 +29,28 @@ class MainsTest < ApplicationSystemTestCase
     fill_in "Password", with: 123456
     click_button "Login"
     assert_selector id:"alert" , text:"Wrong password"
+  end
+
+
+
+
+  #create follow already in follows.yml
+  test "follow post is show" do
+    visit "/main"
+    fill_in "Email", with: @user.email
+    fill_in "Password", with: 12345
+    click_button "Login"
+    assert_selector "h1" , text:"Welcome ".concat(@user.name)
+    assert_text "lebron"
+    assert_text "test"
+  end
+
+  test "Likework" do
+    visit "/main"
+    fill_in "Email", with: @user.email
+    fill_in "Password", with: 12345
+    click_button "Login"
+    click_on "Like" , match: :first
   end
 
 end
